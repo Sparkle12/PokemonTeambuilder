fa57e04571a0b9ed67f238b6c61139ffb46a4aef
Sparkle12 <50665469+Sparkle12@users.noreply.github.com>
2023-12-26 16:16:24 +0200

    Add GameServer files part 2

---
+++ GameServer/Controllers/AuthentificationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Services;
+using SharedLibrary.Requests;
+using SharedLibrary.Responses;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthentificationController : ControllerBase
+    {
+        private readonly IAuthentificationService _authServ;
+
+        public AuthentificationController(IAuthentificationService authServ ) 
+        {
+            _authServ = authServ;
+        }
+        [HttpPost("register")]
+        public IActionResult Register(AuthentificationRequest request)
+        {
+            var (succes, content) = _authServ.Register(request.Username, request.Password);
+
+            if (!succes) return BadRequest(content);
+
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+
+        public IActionResult Login(AuthentificationRequest request)
+        {
+            var (succes, content) = _authServ.Register(request.Username, request.Password);
+
+            if (!succes) return BadRequest(content);
+
+            return Ok(new AuthentificationResponse() { Token = content});
+        }
+    }
+}
